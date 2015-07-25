3f293061811e08c2c2c095b9e898a43e5f70f930
Theo Belaire <theo.belaire@gmail.com>
2015-07-25 17:49:01 -0400

    Added Eliwood and have him walk around

--- loadmap.py
+++ loadmap.py
@@ -12,7 +12,17 @@
 import pygame
 from pytmx.util_pygame import load_pygame
 
+class Player:
+    def __init__(self, filename):
+        self.x = 0
+        self.y = 0
+        self.image = pygame.image.load(filename)
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
 
+
+
+eliwood = Player("resources/FE7_Eliwood_Lord_Map_Sprite1.png")
 # Define some colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -37,16 +47,31 @@ background_position = [0, 0]
 
 done = False
 
+eliwood.y = 24 - 16 - 16
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                eliwood.x -= 16
+            elif event.key == pygame.K_d:
+                eliwood.x += 16
+            elif event.key == pygame.K_w:
+                eliwood.y -= 16
+            elif event.key == pygame.K_s:
+                eliwood.y += 16
+            elif event.key == pygame.K_ESCAPE:
+                done = True
 
     for layer in mapdata:
         for x, y, tile in layer.tiles():
             px = x * mapdata.tilewidth
             py = y * mapdata.tileheight
             screen.blit(tile, (px, py))
+
+    eliwood.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
